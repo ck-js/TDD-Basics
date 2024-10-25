4f33e61cae64300f1f1d965d852f3be620a2093c
ck-js <ckentertainmentinfo@gmail.com>
2024-10-25 16:51:30 +0700

    split test and code with function into seperate files and configure test to run jest in package.json

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function sum(a, b) {
-    // This function will be implemented later
     return a + b;
   }
-
-
-
-
-  export { sum }; // Add this line to the end of the file
+  module.exports = sum;
--- script.test.js
+++ script.test.js
@@ -1,15 +1,5 @@
-function sum(a, b) {
-  // This function will be implemented later
-  return a + b;
-}
+const sum = require('./script');
 
-
-
-
-test('sum function should return the sum of two numbers', () => {
+test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-
-// test('subtract function should return the difference between two numbers', () => {
-//   expect(subtract(5, 3)).toBe(2);
-// });
