39e3a120dbd9e03a68b2745207c3807cdbf9d2c5
ck-js <ckentertainmentinfo@gmail.com>
2024-10-26 15:34:28 +0700

    exceptions unit test
    write test for when we want to test a function
    that throws an error using the toThrow() method
    with various techniques

--- script.test.js
+++ script.test.js
@@ -60,3 +60,21 @@ expect(value).toEqual(4);
   test('but there is a stop in Christoph', () => {
     expect('Christoph').toMatch(/stop/)
   })
+
+
+  function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK!');
+  }
+  
+  test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+  
+    // You can also use a string that must be contained in the error message or a regexp
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
+  
+    // Or you can match an exact error message using a regexp like below
+    // expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); // Test fails
+    expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/); // Test pass
+  });
